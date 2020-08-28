5eb6b97db09dbfdbd1a0c02027fc40342c3edf37
Arnab Roy <ar.firefox@gmail.com>
2020-08-28 05:13:39 +0000

    Added Users Module

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     description="Command line user management utility",
     long_description=readme,
     author="Arnab Roy",
-    author_email="ar.firefox@gmail.com"
-    packages=find_packages("src")
+    author_email="ar.firefox@gmail.com",
+    packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=[]
 )
